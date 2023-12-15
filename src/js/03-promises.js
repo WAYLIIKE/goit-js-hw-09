8c742f79038d0dfc8138efccaecb5e1bbd29bd32
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-15 02:11:46 +0100

    complete 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const selectors = {
+  form: document.querySelector('.form'),
+  btn: document.querySelector('button'),
+};
+
+selectors.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  for (let i = 0; i < event.currentTarget.amount.value; i++) {
+    let position = i + 1;
+
+    let passedStep =
+      Number(event.currentTarget.delay.value) +
+      Number(event.currentTarget.step.value) * i;
+
+    createPromise(position, passedStep)
+      .then(value => {
+        Notify.success(value);
+      })
+      .catch(value => {
+        Notify.failure(value);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
